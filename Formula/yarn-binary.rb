10d4f632c83e8a36afbe8a08fd16e288b92f4fc4
Bruce Sun <bruce.sun@tubemogul.com>
2017-09-25 14:41:19 +0800

    Upgrade yarn-binary, add gradle-binary-only and gradle-all.

--- Formula/yarn-binary.rb
+++ Formula/yarn-binary.rb
@@ -1,8 +1,8 @@
 class YarnBinary < Formula
     desc "JavaScript package manager, binary install (without any dependencies)."
     homepage "https://github.com/yarnpkg/yarn"
-    url "https://github.com/yarnpkg/yarn/releases/download/v1.0.2/yarn-v1.0.2.tar.gz"
-    sha256 "8a31f8fa50ab6d5f8852025fb0ea4a50f2f8b82792f060fa99de0acc370b0698"
+    url "https://github.com/yarnpkg/yarn/releases/download/v1.1.0/yarn-v1.1.0.tar.gz"
+    sha256 "171c1f9ee93c488c0d774ac6e9c72649047c3f896277d88d0f805266519430f3"
 
     bottle :unneeded
 
@@ -13,5 +13,12 @@ class YarnBinary < Formula
         bin.install_symlink "#{libexec}/bin/yarn.js" => "yarn"
         bin.install_symlink "#{libexec}/bin/yarn.js" => "yarnpkg"
         inreplace "#{libexec}/package.json", '"installationMethod": "tar"', '"installationMethod": "homebrew"'
+    end
+
+    def caveats; <<-EOS.undent
+        This formula differs from the original one,
+        as it doesn't have any dependencies,
+        and is downloading tarball from github instead of official site.
+        EOS
     end
 end
